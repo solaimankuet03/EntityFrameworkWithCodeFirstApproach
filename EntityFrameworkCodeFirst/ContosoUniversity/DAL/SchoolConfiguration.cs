4a7df8997fa97c8e904af6c5cbcb6f575ae4c8ba
solaiman <solaimankuet03@outlook.com>
2020-11-11 22:47:29 +0600

    DBInterception-checked through logging and transient errors (very much optional i think)

--- EntityFrameworkCodeFirst/ContosoUniversity/DAL/SchoolConfiguration.cs
+++ EntityFrameworkCodeFirst/ContosoUniversity/DAL/SchoolConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
 using System.Data.Entity.SqlServer;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,8 @@ namespace ContosoUniversity.DAL
         public SchoolConfiguration()
         {
             SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
         }
     }
 }
