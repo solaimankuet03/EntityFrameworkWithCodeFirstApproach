5064e56e95861299e1687ac291ff8d73a62b8a34
solaiman <solaimankuet03@outlook.com>
2020-11-15 21:53:16 +0600

    advanced: Calling an Update Query

--- EntityFrameworkCodeFirst/ContosoUniversity/Controllers/CourseController.cs
+++ EntityFrameworkCodeFirst/ContosoUniversity/Controllers/CourseController.cs
@@ -145,6 +145,21 @@ namespace ContosoUniversity.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult UpdateCourseCredits()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCourseCredits(int? multiplier)
+        {
+            if (multiplier != null)
+            {
+                ViewBag.RowsAffected = db.Database.ExecuteSqlCommand("UPDATE Course SET Credits = Credits * {0}", multiplier);
+            }
+            return View();
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
